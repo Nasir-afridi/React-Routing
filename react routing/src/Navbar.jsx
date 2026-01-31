953f372ae3603b6919642ee13fab38220e39822d
Nasir-afridi <nasirwc05@gmail.com>
2026-01-31 22:07:54 +0500

    Added the navbar with the routing

---
+++ react routing/src/Navbar.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router";
+
+const Links = () => {
+  return (
+    <div className="header">
+      <div>
+        <Link to="/" className="link">
+          <h1>Logo</h1>
+        </Link>
+      </div>
+      <div>
+        <ul className="li-links">
+          <li>
+            <Link to="/" className="link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="link">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/services" className="link">
+              Services
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Links;
