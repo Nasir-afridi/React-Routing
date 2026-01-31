e99ebfdc87e7490ea26d5a5d26b78dca75171441
apple <apple@apples-MacBook-Pro.local>
2026-01-31 19:10:20 +0500

    added the simple routes of home and about page without using link tag

---
+++ react routing/src/App.jsx
@@ -0,0 +1,17 @@
+import {Route, Routes} from 'react-router'
+import Home from './Home'
+import About from './About'
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/about' element={<About />}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
