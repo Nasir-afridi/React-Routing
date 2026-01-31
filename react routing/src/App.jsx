e46a786af23517d0143e90ce5a61dadde20fb5c7
Nasir-afridi <nasirwc05@gmail.com>
2026-01-31 22:06:47 +0500

    added the new routes and import the services page

--- react routing/src/App.jsx
+++ react routing/src/App.jsx
@@ -1,15 +1,17 @@
 import { Route, Routes } from "react-router";
 import Home from "./Home";
 import About from "./About";
-import Links from "./Links";
+import Services from "./Services";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <>
-    <Links />
+    <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />}/>
       </Routes>
     </>
   );
