8caa7cd80f3a5bf9762adb3a8e31755e4e8a3fa3
Nasir-afridi <nasirwc05@gmail.com>
2026-01-31 21:32:53 +0500

    Links navigation file

---
+++ react routing/src/Links.jsx
@@ -0,0 +1,12 @@
+import {Link} from 'react-router'
+
+const Links = () => {
+  return (
+    <div>
+       <Link to="/">Home</Link><br />
+      <Link to="/about">About</Link> 
+    </div>
+  )
+}
+
+export default Links
