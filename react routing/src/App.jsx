9fbd05f091db77fbd93a85b748e96db20863efd8
Nasir-afridi <nasirwc05@gmail.com>
2026-02-02 01:26:57 +0500

    import the pages and add the nested routes

--- react routing/src/App.jsx
+++ react routing/src/App.jsx
@@ -4,18 +4,28 @@ import About from "./About";
 import Services from "./Services";
 import Navbar from "./Navbar";
 import College from "./College";
+import Student from "./Student";
 import PageNotFound from "./PageNotFound";
+import Details from "./Details";
+import Department from "./Department";
 
 function App() {
   return (
     <>
-    <Navbar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />}/>
-        <Route path="/college" element={<College />}/>
-        <Route path="/*" element={<PageNotFound />}/>
+        <Route path="/services" element={<Services />} />
+
+        {/* Nested Routes */}
+        <Route path="/college" element={<College />}>
+          <Route path="student" element={<Student />} />
+          <Route path="details" element={<Details />} />
+          <Route path="department" element={<Department />} />
+        </Route>
+
+        <Route path="/*" element={<PageNotFound />} />
       </Routes>
     </>
   );
