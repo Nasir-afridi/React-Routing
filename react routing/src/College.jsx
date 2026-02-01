6c05ac80e48711e74e0d84da1c3e95cb2ac47359
Nasir-afridi <nasirwc05@gmail.com>
2026-02-02 01:27:30 +0500

    added the Navlinks of the college page

--- react routing/src/College.jsx
+++ react routing/src/College.jsx
@@ -1,8 +1,14 @@
+import {Link, NavLink, Outlet} from 'react-router'
+
 function College() {
   return (
     <>
       <center>
         <h1>College Page</h1>
+        <NavLink to="student" className="Links">Student</NavLink>
+        <NavLink to="details" className="Links">Details</NavLink>
+        <NavLink to="department" className="Links">Department</NavLink>
+        <Outlet></Outlet>
       </center>
     </>
   );
